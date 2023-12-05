e63467c863e1e3e8e72aa274e3f104cbf9799f74
Simeon Filipov <ssfilipov@gmail.com>
2023-12-05 16:11:12 +0200

    day 5 part 2

--- day5.py
+++ day5.py
@@ -2,12 +2,11 @@ from typing import NamedTuple
 
 
 class Range(NamedTuple):
-    src_start: int
-    src_end: int
-    diff: int
+    start: int
+    end: int
 
 
-def parse_map(lines: list[str]) -> list[Range]:
+def parse_map(lines: list[str]) -> list[tuple[Range, int]]:
     result = []
     for line in lines:
         if not line:
@@ -15,16 +14,67 @@ def parse_map(lines: list[str]) -> list[Range]:
         if not line[0].isdigit():
             continue
         dest_start, src_start, r_len = map(int, line.split())
-        result.append(Range(src_start, src_start + r_len - 1, dest_start - src_start))
+        result.append((Range(src_start, src_start + r_len - 1), dest_start - src_start))
 
     return sorted(result)
 
 
-def remap_number(ranges: list[Range], number: int) -> int:
-    for r in ranges:
-        if r.src_start <= number <= r.src_end:
-            return number + r.diff
-    return number
+def intersect(src: Range, dest: Range):
+    if src.end < dest.start:
+        return (src, None, None)
+    if src.start > dest.end:
+        return (None, None, src)
+
+    before = Range(src.start, dest.start - 1) if src.start < dest.start else None
+    inter = Range(max(src.start, dest.start), min(src.end, dest.end))
+    after = Range(dest.end + 1, src.end) if src.end > dest.end else None
+    return (before, inter, after)
+
+
+def remap_range(src_ranges: list[Range], dest_ranges: list[tuple[Range, int]]) -> list[Range]:
+    if not src_ranges:
+        return []
+
+    result: list[Range] = []
+    i = 0
+    j = 0
+    current_src = None
+    while i < len(src_ranges) or current_src:
+        if not current_src:
+            current_src = src_ranges[i]
+
+        if j < len(dest_ranges):
+            current_dest, diff = dest_ranges[j]
+            before, inter, after = intersect(current_src, current_dest)
+            if inter:
+                if before:
+                    result.append(before)
+
+                result.append(Range(inter.start + diff, inter.end + diff))
+
+                if after:
+                    current_src = after
+                    j += 1
+                else:
+                    current_src = None
+                    i += 1
+
+            else:
+                if before:
+                    result.append(before)
+                    current_src = None
+                    i += 1
+
+                if after:
+                    current_src = after
+                    j += 1
+
+        else:
+            result.append(current_src)
+            current_src = None
+            i += 1
+
+    return sorted(result)
 
 
 def main():
@@ -40,18 +90,26 @@ def main():
     temp_to_hum = parse_map(next(blocks))
     hum_to_loc = parse_map(next(blocks))
 
-    locations = []
-    for seed in seeds:
-        soil = remap_number(seed_to_soil, seed)
-        fer = remap_number(soil_to_fer, soil)
-        water = remap_number(fer_to_water, fer)
-        light = remap_number(water_to_light, water)
-        temp = remap_number(light_to_temp, light)
-        hum = remap_number(temp_to_hum, temp)
-        loc = remap_number(hum_to_loc, hum)
-        locations.append(loc)
+    def transform_seeds(seeds: list[Range]) -> int:
+        soil = remap_range(seeds, seed_to_soil)
+        fer = remap_range(soil, soil_to_fer)
+        water = remap_range(fer, fer_to_water)
+        light = remap_range(water, water_to_light)
+        temp = remap_range(light, light_to_temp)
+        hum = remap_range(temp, temp_to_hum)
+        loc = remap_range(hum, hum_to_loc)
+        return loc[0].start
 
-    print(sorted(locations)[0])
+    seeds_part1 = sorted(Range(seed, seed) for seed in seeds)
+    print(transform_seeds(seeds_part1))
+
+    seeds_part2: list[Range] = []
+    for i in range(0, len(seeds), 2):
+        start, r_len = seeds[i], seeds[i + 1]
+        end = start + r_len - 1
+        seeds_part2.append(Range(start, end))
+    seeds_part2.sort()
+    print(transform_seeds(seeds_part2))
 
 
 if __name__ == '__main__':
